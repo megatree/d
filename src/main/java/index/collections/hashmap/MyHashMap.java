15a0477505b34f776fabd2b27aebf74fbd6f1203
wangzhe.bj <wangzhe.bj@fang.com>
2017-11-13 18:10:34 +0800

    初始化hashmap

---
+++ src/main/java/index/collections/hashmap/MyHashMap.java
@@ -0,0 +1,39 @@
+package index.collections.hashmap;
+
+import java.io.Serializable;
+
+/**
+ * Created by wangzhe.bj on 2017/11/13.
+ */
+public class MyHashMap<K, V> implements Cloneable, Serializable {
+
+
+    public int size() {
+        return 0;
+    }
+
+
+    public boolean isEmpty() {
+        return false;
+    }
+
+
+    public V get(Object key) {
+        return null;
+    }
+
+
+    public V put(K key, V value) {
+        return null;
+    }
+
+
+    public V remove(Object key) {
+        return null;
+    }
+
+
+    public void clear() {
+
+    }
+}
