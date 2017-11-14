b46f8135ea1b2faacf828b742aa413db3fe4255c
wangzhe.bj <wangzhe.bj@fang.com>
2017-11-14 19:52:24 +0800

    大数

--- src/main/java/index/questions/BigInteger.java
+++ src/main/java/index/questions/BigInteger.java
@@ -6,8 +6,29 @@ package index.questions;
 public class BigInteger {
 
     public static void main(String[] args) {
-
-
+        String s1 = "1111111";
+        String s2 = "222";
+        add(s1, s2);
 
     }
+
+    public static String add(String a, String b) {
+        int len1 = a.length();
+        int len2 = b.length();
+
+        int maxLen = Math.max(len1, len2);
+        int[] mids = new int[maxLen];
+        for (int i = maxLen - 1; i >= 0; i--) {
+            int ix = maxLen - i - 1;
+
+            if (ix < len1) {
+
+            }
+        }
+
+
+        return null;
+    }
+
+
 }
