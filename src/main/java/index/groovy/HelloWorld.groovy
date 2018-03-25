5b291d5fe2b9456390852c81a59ecf8d1c22c3ce
naiive <mythss@foxmail.com>
2018-03-26 01:10:02 +0800

    groovy 练习

--- src/main/java/index/groovy/HelloWorld.groovy
+++ src/main/java/index/groovy/HelloWorld.groovy
@@ -5,11 +5,87 @@ package index.groovy
  *
  */
 
-findd()
+
+trait Readable {
+    void read() {
+        println("read...")
+    }
+}
+
+trait Writable {
+    void write(String text) {
+        println("write $text")
+    }
+}
+
+class Notebook implements Readable, Writable {
+    static void main(String[] args) {
+        //使用特性类
+        def notebook = new Notebook()
+        notebook.read()
+        notebook.write("something")
+    }
+}
+
+def (a,b,c) = [1,'b',null]
+
+Integer x = "1"
+String y = x as int
+
+
+def list1 = ['Groovy 1.8','Groovy 2.0','Groovy 2.3']
+def list2 = ['Groovy 1.8','Groovy 2.0','Groovy 2.3']
+
+
+def cp = '123'as int <=> '12' as int
+
+def arr = (1..<9).collect()
+def bool = null in arr
+
+def users = [new User("1"), new User("1"), new User(null)]
+def uarr = users*.@name.findAll { it != null }
+
+
+def classTest() {
+    def user = new User("hello")
+    println user.name
+    println user.@name
+}
+
+class User {
+
+    public final String name
+
+    User(String name) {
+        this.name = name
+    }
+
+    String getName() {
+        "name-> $name"
+    }
+
+    boolean equals(o) {
+        if (this.is(o)) return true
+        if (getClass() != o.class) return false
+
+        User user = (User) o
+
+        if (name != user.name) return false
+
+        return true
+    }
+
+    int hashCode() {
+        return (name != null ? name.hashCode() : 0)
+    }
+}
+
 
 def findd() {
     def map = [k: 1, l: 2]
     def o = map.find { it.getKey() == 'l' }
+    def p = map.find { it.getValue().toString().equals("hehe") }
+
     println o
 
     map << [j: 3]
---
+++ src/main/java/index/groovy/Mytest.groovy
@@ -0,0 +1,9 @@
+package index.groovy
+
+/**
+ * Created by mythss on 2018/3/26.
+ */
+
+
+
+
