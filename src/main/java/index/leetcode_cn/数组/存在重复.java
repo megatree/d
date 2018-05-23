c91acd5944c47df65b6e9f6c57ca8f42543339dd
wangzhe.bj <wangzhe.bj@fang.com>
2018-05-23 13:45:29 +0800

    例行更新

--- src/main/java/index/leetcode_cn/数组/存在重复.java
+++ src/main/java/index/leetcode_cn/数组/存在重复.java
@@ -17,8 +17,15 @@ public class 存在重复 {
         int[] arr1 = {1, 1, 1, 3, 3, 4, 3, 2, 4, 2};
         int[] arr2 = {1, 2, 3, 4};
 
-        assert containsDuplicate(arr1);
-        assert !containsDuplicate(arr2);
+//        assert containsDuplicate(arr1);
+//        assert !containsDuplicate(arr2);
+
+        int[] arr3 = {9, 3, 6, 1, 10, 0, 9};
+//        assert containsDuplicate_copy(arr3);
+
+        assert containsDuplicate(arr3);
+
+
     }
 
     public boolean containsDuplicate(int[] nums) {
@@ -31,6 +38,26 @@ public class 存在重复 {
         for (int i = 1; i < nums.length; i++) {
             if (nums[i] == nums[i - 1]) {
                 return true;
+            }
+        }
+        return false;
+    }
+
+
+    public boolean containsDuplicate_copy(int[] nums) {
+        if (nums.length < 2) {
+            return false;
+        }
+
+        for (int i = 1; i < nums.length; i++) {
+            for (int j = i - 1; j >= 0; j--) {
+                if (nums[i] > nums[j] && j + 1 != i) {
+
+                    break;
+                }
+                if (nums[i] == nums[j]) {
+                    return true;
+                }
             }
         }
         return false;
--- src/main/java/index/leetcode_cn中级/数组和字符串/最长回文子串.java
+++ src/main/java/index/leetcode_cn中级/数组和字符串/最长回文子串.java
@@ -24,10 +24,96 @@ public class 最长回文子串 {
     @Test
     public void go() {
 
+//        System.out.println(longestPalindrome("babad"));
+//        System.out.println(longestPalindrome("cbbd"));
+        System.out.println(longestPalindrome("abb"));
     }
 
     public String longestPalindrome(String s) {
+        int len = s.length();
+        if (len < 2) {
+            return s;
+        }
 
+        //填充#，使其每一个子序列都为奇数，存在中点
+        StringBuilder sb = new StringBuilder();
+        sb.append("#");
+        for (int i = 0; i < len; i++) {
+            sb.append(s.charAt(i)).append("#");
+        }
+
+        String arr = sb.toString();
+        int arrLen = arr.length();
+        String maxStr = "";
+        int maxLen = 0;
+
+        int index = 0;
+        while (index < arrLen) {
+            String temp = outwardScanning(arr, index, 1, arrLen - 1, maxLen);
+            if (temp != null) {
+                maxLen = temp.length();
+                maxStr = temp;
+            }
+
+            //在这里优化，向右遍历，滤掉无效循环
+            index++;
+        }
+
+        return maxStr;
+    }
+
+    /**
+     * 由中心向外层扫描，返回去除#的字符串
+     *
+     * @param src
+     * @param midIndex
+     * @param leftBorder
+     * @param rightBorder
+     * @return
+     */
+    public String outwardScanning(String src, int midIndex, int leftBorder, int rightBorder, int maxLen) {
+
+        int left = midIndex - 1;
+        int right = midIndex + 1;
+        int curPalinLength = src.charAt(midIndex) == '#' ? 0 : 1;
+
+
+        while (left >= leftBorder && right <= rightBorder) {
+            //此时结束回文
+            if (src.charAt(left) != src.charAt(right)) {
+                left++;
+                right--;
+                break;
+            }
+
+            //此处left right 相等
+            if (src.charAt(left) != '#') {
+                curPalinLength += 2;
+            }
+
+            left--;
+            right++;
+        }
+
+        //当前回文长度确定，与最大回文长度比较
+        if (curPalinLength > maxLen) {
+            StringBuilder sb = new StringBuilder();
+
+            //将left right 回退到合法位置
+            while (!(left >= leftBorder && right <= rightBorder)) {
+                left++;
+                right--;
+            }
+
+            for (int i = left; i <= right; i++) {
+                if (src.charAt(i) != '#') {
+                    sb.append(src.charAt(i));
+                }
+            }
+            return sb.toString();
+        }
+
+        return null;
     }
 
 }
