8f03db665f27ca4871de0590693be68fa1f2acd5
naiive <mythss@foxmail.com>
2018-06-13 00:40:22 +0800

    例行更新

--- src/main/java/index/leetcode_cn高级/数组和字符串/四数相加II.java
+++ src/main/java/index/leetcode_cn高级/数组和字符串/四数相加II.java
@@ -2,23 +2,57 @@ package index.leetcode_cn高级.数组和字符串;
 
 import org.junit.Test;
 
-import java.sql.PreparedStatement;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by wangzhe.bj on 2018-06-06.
- *
+ * <p>
  * https://leetcode-cn.com/explore/interview/card/top-interview-questions-hard/55/array-and-strings/125/
- *
- *
  */
 public class 四数相加II {
 
     @Test
-    public void go(){
+    public void go() {
+        int[] A = {1, 2};
+        int[] B = {-2, -1};
+        int[] C = {-1, 2};
+        int[] D = {0, 2};
 
+        assert 2 == fourSumCount(A, B, C, D);
     }
 
+    /**
+     * 前两个数组合并，将遍历求和结果放入map。并在后两个数组中寻找其相反数
+     * 129ms 93.49%
+     *
+     * @param A
+     * @param B
+     * @param C
+     * @param D
+     * @return
+     */
     public int fourSumCount(int[] A, int[] B, int[] C, int[] D) {
+        if (A.length==0){
+            return 0;
+        }
 
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < A.length; i++) {
+            for (int i1 = 0; i1 < B.length; i1++) {
+                //每个组合计数
+                map.put(A[i] + B[i1], map.getOrDefault(A[i] + B[i1], 0) + 1);
+            }
+        }
+
+        int count = 0;
+        for (int i = 0; i < C.length; i++) {
+            for (int i1 = 0; i1 < D.length; i1++) {
+                count += map.getOrDefault(-C[i] - D[i1], 0);
+            }
+        }
+
+        return count;
     }
 }
---
+++ src/main/java/index/leetcode_cn高级/数组和字符串/盛最多水的容器.java
@@ -0,0 +1,30 @@
+package index.leetcode_cn高级.数组和字符串;
+
+import org.junit.Test;
+
+/**
+ * Created by mythss on 2018-06-13.
+ * <p>
+ * https://leetcode-cn.com/explore/interview/card/top-interview-questions-hard/55/array-and-strings/126/
+ * <p>
+ * <p>
+ * 给定 n 个非负整数 a1，a2，...，an，每个数代表坐标中的一个点 (i, ai) 。画 n 条垂直线，使得垂直线 i 的两个端点分别为 (i, ai) 和 (i, 0)。找出其中的两条线，使得它们与 x 轴共同构成的容器可以容纳最多的水。
+ * <p>
+ * 注意：你不能倾斜容器，n 至少是2。
+ */
+public class 盛最多水的容器 {
+
+    @Test
+    public void go() {
+
+    }
+
+    /**
+     * 木桶理论
+     * @param height
+     * @return
+     */
+    public int maxArea(int[] height) {
+
+    }
+}
