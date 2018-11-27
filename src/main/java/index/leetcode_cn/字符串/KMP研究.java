28bc020ab3d6c7b3ca52eed60ca2e22dbfbd3ed8
wangzhe.bj <wangzhe.bj@fang.com>
2018-11-27 17:54:10 +0800

    例行更新

--- src/main/java/index/leetcode_cn/字符串/KMP研究.java
+++ src/main/java/index/leetcode_cn/字符串/KMP研究.java
@@ -6,41 +6,46 @@ import java.util.Arrays;
 
 /**
  * Created by wangzhe.bj on 2018-11-23.
+ * <p>
+ * //    作者：逍遥行
+ * //    链接：https://www.zhihu.com/question/21923021/answer/37475572
+ * //https://segmentfault.com/a/1190000008575379
+ * //https://segmentfault.com/a/1190000007066358
  */
 public class KMP研究 {
 
     @Test
     public void test() {
-        String a = "abcdaba";
-        int[] next = next(a);
+        String a = "abcdabc";
+        int[] next = getNext(a);
         System.out.println(Arrays.toString(next));
     }
 
     /**
      * 构造模式串的最大匹配数表
      *
-     * @param p
+     * @param ps
      * @return
      */
-    int[] next(String p) {
-        int[] next = new int[p.length()];
-        //迭代过程中，模式串的前缀数组长度
-        int len = 0;
+    public int[] getNext(String ps) {
 
-        for (int i = 1; i < p.length(); i++) {
-            while (len > 0 && p.charAt(len) != p.charAt(i)) {
-                len = next[len - 1];
+        char[] p = ps.toCharArray();
+        int[] next = new int[p.length];
+
+        next[0] = -1;
+        int j = 0;
+        int k = -1;
+
+        while (j < p.length - 1) {
+            if (k == -1 || p[j] == p[k]) {
+
+                next[++j] = ++k;
+            } else {
+                k = next[k];
             }
-            if (p.charAt(i) == p.charAt(len)) {
-                len++;
-            }
-            next[i] = len;
         }
         return next;
+
     }
 
-//    作者：逍遥行
-//    链接：https://www.zhihu.com/question/21923021/answer/37475572
-    //https://segmentfault.com/a/1190000008575379
-    //https://segmentfault.com/a/1190000007066358
 }
